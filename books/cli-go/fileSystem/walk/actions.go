6b91814cfdbfa1c6e51647e11e99f6af5f3070bf
Adam Wolf <adam@adamjwolf.com>
2020-11-11 14:34:51 -0600

    Add delete to walk cli

--- books/cli-go/fileSystem/walk/actions.go
+++ books/cli-go/fileSystem/walk/actions.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io"
+
+	"log"
 	"os"
 	"path/filepath"
 )
@@ -21,4 +23,13 @@ func filterOut(path, ext string, minSize int64, info os.FileInfo) bool {
 func listFile(path string, out io.Writer) error {
 	_, err := fmt.Fprintln(out, path)
 	return err
+}
+
+func delFile(path string, delLogger *log.Logger) error {
+	if err := os.Remove(path); err != nil {
+		return err
+	}
+
+	delLogger.Println(path)
+	return nil
 }
